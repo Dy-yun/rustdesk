9e445db150d7a22b21f43a1464c9b52c42adfd9c
dy <150418403@qq.com>
2026-01-08 00:09:42 +0800

    更换API地址，修改广告语

--- src/common.rs
+++ src/common.rs
@@ -1058,7 +1058,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://home.dymode.com:21114".to_owned()
 }
 
 #[inline]
